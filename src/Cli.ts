a560b5ce9900cbee33d0e7bac2809299edaed6a9
Fredrik Lindin <fredriklindin@gmail.com>
2021-12-04 23:45:54 +0100

    Support multiple schemas that merges into one.
    Relies on globals. So if `type User` is defined multiple times... Well
    tough luck. But will add some validations for it.

--- src/Cli.ts
+++ src/Cli.ts
@@ -1,51 +1,80 @@
 import chalk from 'chalk';
 import { program } from 'commander';
 import fs from 'fs';
-import ApiParser, { ApiSyntaxError } from './ApiParser';
+import path from 'path';
+import ApiParser, { ApiSyntaxError, Ast } from './ApiParser';
 import generateFastify from './generators/generateFastify';
 import PrettyError from './PrettyError';
 
 program
   .description('generate fastify plugin')
-  .argument('<input-file>', 'api schema file')
   .argument('<output-file>', 'generated typescript file')
+  .argument('<input-file...>', '1 or more api schema files. Will be merged into 1.')
   .option('-w --watch', 'watch for changes', false)
-  .action(async (inputFile: string, outputFile: string, { watch }: { watch: boolean }) => {
+  .action(async (outputFile: string, inputFiles: string[], { watch }: { watch: boolean }) => {
     const generate = (exit = false) => {
-      try {
-        const measure = new Date().getTime();
-        const schema = fs.readFileSync(inputFile, 'utf8');
-        const parser = new ApiParser();
-        const ast = parser.parse(schema);
-        const outputData = generateFastify(ast);
+      const measure = new Date().getTime();
+      const mergedAst: Ast = {
+        type: 'Document',
+        definitions: [],
+      };
+
+      let write = true;
+      for (const inputFile of inputFiles) {
+        try {
+          const schema = fs.readFileSync(inputFile, 'utf8');
+          const parser = new ApiParser();
+          const ast = parser.parse(schema);
+          mergedAst.definitions = [...mergedAst.definitions, ...ast.definitions];
+        } catch (err: unknown) {
+          if (err instanceof ApiSyntaxError) {
+            console.log(
+              `${chalk.red(path.join(process.cwd(), inputFile))}:${chalk.yellow(err.token.line)}:${chalk.yellow(
+                err.token.col + 1,
+              )}`,
+            );
+            console.log(
+              PrettyError({
+                token: err.token,
+                document: err.document,
+                errorMessage: err.message,
+              }),
+            );
+          } else {
+            console.log(err);
+          }
+          write = false;
+        }
+      }
+
+      if (write) {
+        const outputData = generateFastify(mergedAst);
         fs.writeFileSync(outputFile, outputData, 'utf8');
         console.log(` ${chalk.green.bold('✓')} ${outputFile} ${chalk.green(`${new Date().getTime() - measure}ms`)}`);
-      } catch (err: unknown) {
-        if (err instanceof ApiSyntaxError) {
-          console.log(
-            PrettyError({
-              token: err.token,
-              document: err.document,
-              errorMessage: err.message,
-            }),
-          );
-        } else {
-          console.log(err);
-        }
+      } else {
         if (exit) {
           process.exit(1);
         }
       }
     };
     if (watch) {
-      const watcher = fs.promises.watch(inputFile);
-      console.log(`${chalk.bold('WATCH')} ${inputFile}`);
+      const watchAll = async function () {
+        return Promise.all(
+          inputFiles.map((file) => {
+            console.log(`${chalk.bold('WATCH')} ${file}`);
+            return fs.promises.watch(file);
+          }),
+        );
+      };
+
       generate();
-      for await (const event of watcher) {
-        if (event.eventType === 'change') {
-          generate();
+      (await watchAll()).map(async (x) => {
+        for await (const event of x) {
+          if (event.eventType === 'change') {
+            generate();
+          }
         }
-      }
+      });
     } else {
       generate(true);
     }
